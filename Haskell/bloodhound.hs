248e8b834e5436a5f177780eae48cc41de88c9a3
Joao Sobral <joao.sobral@ccc.ufcg.edu.br>
2019-10-01 14:46:18 -0300

    enviando o main

--- Haskell/bloodhound.hs
+++ Haskell/bloodhound.hs
@@ -1,3 +1,6 @@
+import System.random 
+
+
 data Tuple = JOGADOR [String] [String] [String] [String] [String] |
              DADOS [String] [String] [String] |
              RESPOSTA String String String
@@ -5,19 +8,36 @@ data Tuple = JOGADOR [String] [String] [String] [String] [String] |
 getLugar :: Tuple -> String
 getPessoa :: Tuple -> String
 getArma :: Tuple -> String
+getLugares :: Tuple -> [String]
+getArmas :: Tuple -> [String]
+getPessoas :: Tuple -> [String]
+remove :: String -> [String] -> [String]
+
 
 getLugar (RESPOSTA lugar arma pessoa) = lugar
 getArma (RESPOSTA lugar arma pessoa) = arma
 getPessoa (RESPOSTA lugar arma pessoa) = pessoa
-let lugares = ["Lavanderia", "Banco" , "Estadio", "Cinema", "Floresta", "Escola", "Igreja", "Shopping", "Praia"]
-let armas = ["Machado", "Pa", "Arma quimica", "Revolver", "Faca", "Pe de cabra", "Veneno", "Corda", "Tesoura"]
-let pessoas = ["Johann", "Matias", "Clarisse", "Alfred", "Jasmine", "Rosa", "Taylor", "Solomon", "Viktor"]
 
-
-
-
-
-
-remove :: String -> [String] -> [String]
+getLugares (DADOS lugares armas pessoas) = lugares 
+getArmas (DADOS lugares armas pessoas) = armas 
+getPessoas (DADOS lugares armas pessoas) = pessoas 
 
 remove element list = filter (\e -> e/=element) list
+
+main = do 
+    let lugares = ["Lavanderia", "Banco" , "Estadio", "Cinema", "Floresta", "Escola", "Igreja", "Shopping", "Praia"]
+    let armas = ["Machado", "Pa", "Arma quimica", "Revolver", "Faca", "Pe de cabra", "Veneno", "Corda", "Tesoura"]
+    let pessoas = ["Johann", "Matias", "Clarisse", "Alfred", "Jasmine", "Rosa", "Taylor", "Solomon", "Viktor"]
+    let prioridades = ["", "", ""]
+    let cartas = []
+
+    base = DADOS lugares armas pessoas
+    pessoa = JOGADOR lugares armas pessoas prioridades cartas
+    bot1 = JOGADOR lugares armas pessoas prioridades cartas
+    bot2 = JOGADOR lugares armas pessoas prioridades cartas
+
+    
+
+
+
+
