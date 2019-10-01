2e968b631e98606bfdebf2a4d8efd8bb67f10bd4
luismafra <luis.mafra@ccc.ufcg.edu.br>
2019-10-01 15:54:19 -0300

    commit

--- Haskell/bloodhound.hs
+++ Haskell/bloodhound.hs
@@ -1,4 +1,4 @@
-import System.random 
+import System.Random(randomRIO) 
 
 
 data Tuple = JOGADOR [String] [String] [String] [String] [String] |
@@ -12,7 +12,8 @@ getLugares :: Tuple -> [String]
 getArmas :: Tuple -> [String]
 getPessoas :: Tuple -> [String]
 remove :: String -> [String] -> [String]
-
+sorteiaResposta :: Tuple -> IO Tuple
+criaBaralho :: Tuple -> Tuple -> IO [String]
 
 getLugar (RESPOSTA lugar arma pessoa) = lugar
 getArma (RESPOSTA lugar arma pessoa) = arma
@@ -22,7 +23,22 @@ getLugares (DADOS lugares armas pessoas) = lugares
 getArmas (DADOS lugares armas pessoas) = armas 
 getPessoas (DADOS lugares armas pessoas) = pessoas 
 
-remove element list = filter (\e -> e/=element) list
+remove element list = filter (/=element) list
+sorteiaResposta(DADOS lugares armas pessoas) = do
+    x <- randomRIO(0,8::Int)
+    y <- randomRIO(0,8::Int)
+    z <- randomRIO(0,8::Int)
+    let resposta = RESPOSTA (lugares!!x) (armas!!y) (pessoas!!z)
+    return resposta
+
+criaBaralho (DADOS lugares armas pessoas) (RESPOSTA lugar arma pessoa) = do
+    let tudo = lugares ++ armas ++ pessoas
+    let tudo2 = remove lugar tudo
+    let tudo3 = remove arma tudo2
+    let tudo4 = remove pessoa tudo3
+    return tudo4
+
+
 
 main = do 
     let lugares = ["Lavanderia", "Banco" , "Estadio", "Cinema", "Floresta", "Escola", "Igreja", "Shopping", "Praia"]
@@ -31,12 +47,16 @@ main = do
     let prioridades = ["", "", ""]
     let cartas = []
 
-    base = DADOS lugares armas pessoas
-    pessoa = JOGADOR lugares armas pessoas prioridades cartas
-    bot1 = JOGADOR lugares armas pessoas prioridades cartas
-    bot2 = JOGADOR lugares armas pessoas prioridades cartas
-
-    
+    let base = DADOS lugares armas pessoas
+    let pessoa = JOGADOR lugares armas pessoas prioridades cartas
+    let bot1 = JOGADOR lugares armas pessoas prioridades cartas
+    let bot2 = JOGADOR lugares armas pessoas prioridades cartas
+    resposta <- sorteiaResposta base
+    baralho <- criaBaralho base resposta
+    print(getArma resposta)
+    print(getLugar resposta)
+    print(getPessoa resposta)
+    putStrLn $ show baralho
 
 
 
